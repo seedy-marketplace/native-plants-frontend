cc4d13c0ffd73c1f07071cdf194198457cb75192
LemonTorrent <nyarbrough14@gmail.com>
2023-03-16 21:28:21 -0700

    Continuing to try to figure out ITIS calls

--- src/pages/api/accessITIS.js
+++ src/pages/api/accessITIS.js
@@ -1,14 +1,29 @@
+
 export default async function (req, res) {
     if (req.method === 'SEARCH') {
         var ret = await accessItis(req.body["species_name"], req.body["tsn"])
-        res.status(200).json({
+        res.status(200).send({
             ret:ret
         })
     }
 }
 
 async function accessItis(species_name, tsn){
-    fetch(("https://www.itis.gov/ITISWebService/services/ITISService/searchByScientificName?srchKey="+species_name), {
+    // var url = "https://www.itis.gov/ITISWebService/services/ITISService/searchByScientificName?srchKey="+species_name
+    var url = "https://www.itis.gov/ITISWebService/services/ITISService/searchByScientificName?srchKey=none"
+    
+    var XMLHttpRequest = require('xhr2');
+    const req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.overrideMimeType("text/plain; charset=x-user-defined");
+    console.log("XMLHTTPREQUEST: ",req)
+    req.onload = (e) => {
+        const arraybuffer = req.response; // not responseText
+        console.log("Array buffer:", arraybuffer)
+      };
+      
+
+    fetch((url), {
                     headers: {
                         'Content-Type': 'application/json',
                         // 'mode': 'no-cors',
@@ -18,23 +33,23 @@ async function accessItis(species_name, tsn){
 
                 })
                     .then((res) => {
-                        var retVal = res.json()
+                        // var retVal = res.json()
                         console.log("Res: ", res)
-                        return retVal
+                        return res
                     })
-                    .then(
-                        (result) => {
-                            console.log("Results:", result)
-                        //   setIsLoaded(true);
-                        //   setItems(result);
-                        },
-                        // Note: it's important to handle errors here
-                        // instead of a catch() block so that we don't swallow
-                        // exceptions from actual bugs in components.
-                        (error) => {
-                        //   setIsLoaded(true);
-                          console.log("Error with accessing ITIS, ", error)
-                        }
-                    )
+                    // .then(
+                    //     (result) => {
+                    //         console.log("Results:", result)
+                    //     //   setIsLoaded(true);
+                    //     //   setItems(result);
+                    //     },
+                    //     // Note: it's important to handle errors here
+                    //     // instead of a catch() block so that we don't swallow
+                    //     // exceptions from actual bugs in components.
+                    //     (error) => {
+                    //     //   setIsLoaded(true);
+                    //       console.log("Error with accessing ITIS, ", error)
+                    //     }
+                    // )
 
 }
