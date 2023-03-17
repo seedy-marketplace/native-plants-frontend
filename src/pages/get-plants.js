36f9739bdd006637eea4d3949f3aedae0fc309db
nat <72530622+bourassn@users.noreply.github.com>
2023-03-17 02:00:34 -0700

    searches and combining plant, collection, and user tables

--- src/pages/get-plants.js
+++ src/pages/get-plants.js
@@ -8,6 +8,8 @@ import TableView from '../components/TableView';
 
 function Plants() {
     const [comname, setComname] = useState("");
+    const [genus, setGenus] = useState("");
+    const [species, setSpecies] = useState("")
     const [email, setEmail] = useState("");
     const [plantList, setPlantList] = useState([]);
 
@@ -16,32 +18,31 @@ function Plants() {
     // const [res, loading, error] = useAPIRequest(`https://native-plants-backend.herokuapp.com/q/SELECT * FROM rev2.farms`, "GET");
     async function getPlants(e) {
         e.preventDefault();
-        // let searchfront = '/api/accessBackend?query_string=SELECT '
-        // let searchback = ' FROM rev2.plant'
-        // var searchmid = '*'
-        // if (comname !=""){
-        //     searchback = searchback + " Where common_name LIKE '" + comname + "'"
-        // }
-        // let searchfinal = searchfront + searchmid + searchback
-        // console.log("== searching this: ", searchfinal);
-        // const res = await fetch(searchfinal,
-        //     {
-        //         method: 'GET',
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         query: "Plants"
-        //     }
-        // )
-        const res = await fetch("/api/accessDatabase",{
+        var onVar = 'plant.species_code = seed_collection.col_species_code';
+        if (comname) {
+            onVar += ` AND plant.common_name iLIKE '%%${comname}%%'`;
+        }
+        if (genus) {
+            onVar += ` AND plant.genus iLIKE '%%${genus}%%'`
+        }
+        if (species) {
+            onVar += ` AND plant.species iLIKE '%%${species}%%'`
+        }
+
+        onVar += ` INNER JOIN rev2.users ON seed_collection.id_person_name = users.user_name`
+
+
+        const res = await fetch("/api/accessDatabase", {
+
             method: 'SEARCH', //SEARCH, POST, DELETE, UPDATE  (NOT GET)
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 query_type: 'SELECT', //SELECT, INSERT, etc. (Field is required)
-                table_name: 'plant', //Any table name here (Field is required)
-                where: `${comname ? `common_name LIKE '%%${comname}%%'` : ""}`
+                table_name: 'seed_collection', //Any table name here (Field is required)
+                columns: ['plant.genus', 'plant.species', 'plant.common_name', 'seed_collection.collected_date', 'seed_collection.id_method', 'seed_collection.id_confidence', 'seed_collection.cleaned_weight', 'seed_collection.cleaning_effectiveness', 'users.email', 'users.phone_number', 'users.website'],
+                join_string: onVar ? ' INNER JOIN rev2.plant ON ' + onVar : null
             })
         })
         const resBody = await res.json();
@@ -65,6 +66,18 @@ function Plants() {
                         placeholder="Search by Common name"
                         onChange={e => setComname(e.target.value)}
                         value={comname}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Search Genus"
+                        onChange={e => setGenus(e.target.value)}
+                        value={genus}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Search Species"
+                        onChange={e => setSpecies(e.target.value)}
+                        value={species}
                     />
                 </div>
                 <div>
