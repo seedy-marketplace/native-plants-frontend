e6693b2aa05c6562f63abe3bc61b138043c0bfd7
LemonTorrent <nyarbrough14@gmail.com>
2023-04-12 17:40:25 -0700

    Removed unecessary comments

--- src/pages/api/accessITIS.js
+++ src/pages/api/accessITIS.js
@@ -12,26 +12,12 @@ export default async function (req, res) {
 }
 
 async function accessItis(species_name, tsn){
-    // var url = "https://www.itis.gov/ITISWebService/services/ITISService/searchByScientificName?srchKey="+species_name
-    // var url = "https://www.itis.gov/ITISWebService/services/ITISService/searchByScientificName?srchKey=procera"
-    // var url = "https://services.itis.gov/"
     var url = "https://services.itis.gov/?q=tsn:182662"
-    
-    // var XMLHttpRequest = require('xhr2');
-    // const req = new XMLHttpRequest();
-    // req.open("GET", url);
-    // req.overrideMimeType("text/plain; charset=x-user-defined");
-    // console.log("XMLHTTPREQUEST: ",req)
-    // req.onload = (e) => {
-    //     const arraybuffer = req.response; // not responseText
-    //     console.log("Array buffer:", arraybuffer)
-    //   };
 
     console.log("AccessItis arguments: species_name=",species_name," tsn=", tsn)
     const fetch = require("node-fetch");
       
 
-    // const response = await fetch(url, {
     const response = await fetch(url, {
 
             method: 'GET',
@@ -40,24 +26,9 @@ async function accessItis(species_name, tsn){
             },
             redirect: 'follow',
         })
-    // const response = await fetch(url)
-        // .then((res) => {
-        //             // console.log("Json: ", res.json())
-        //             // return (res.json())
-        //             console.log("Initial res", res);
-        //             var text = res.json()
-        //             console.log("Res text", text)
-        //             // console.log("Call returns ", res.text());
-        //             // console.log("Call returns ", JSON.parse(text));
-
-        //             return res;
-        //         })
-
         .then(res=>res.json())
                 
     console.log("Response outside fetch:", response)
     console.log("Type of data: ", typeof response)
-    // const responseJson = await response.json
-    // console.log("Response json:", responseJson)
     return response
 }
