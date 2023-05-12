4b12e3f263d0bd25a977d6cda0899ee7e81fe59d
SquidHater1 <smithr01@yahoo.com>
2023-05-12 01:53:32 -0700

    Remove Login from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -53,11 +53,13 @@ const Nav = () => (
                       <a>Nursery</a>
                     </Link>
             </li>
+                 /*
             <li className="nav-list-item">
                     <Link href="/login">
                       <a>Login</a>
                     </Link>
             </li>
+            */
             
             
             <li className="nav-list-item">
