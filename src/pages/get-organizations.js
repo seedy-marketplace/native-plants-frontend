f73ad54fff8a4df903fa0ec7b0c357119be3dbee
Ryan <smithr01@yahoo.com>
2023-05-18 19:51:42 -0700

    Got organization input done

---
+++ src/pages/get-organizations.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Layout from '../components/Layout';
+
+import useAPIRequest from '../hooks/useAPIRequest';
+
+import TableView from '../components/TableView';
+
+function Organization() {
+    const [orgname, setOrgname] = useState("");
+    const [orgList, setOrgList] = useState([]);
+
+    // const [res, loading, error] = useAPIRequest(`https://native-plants-backend.herokuapp.com/i/INSERT INTO rev2.farms(farm_name) VALUES (%s) /${farmname_to_send}`, "POST");
+    // const [res, loading, error] = useAPIRequest(`https://native-plants-backend.herokuapp.com/q/SELECT * FROM rev2.farms`, "GET");
+    async function getOrg(e) {
+        e.preventDefault();
+        
+        const res = await fetch("/api/accessDatabase",{
+            method: 'SEARCH', //SEARCH, POST, DELETE, UPDATE  (NOT GET)
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query_type: 'SELECT', //SELECT, INSERT, etc. (Field is required)
+                table_name: 'organization', //Any table name here (Field is required)
+                where: `${orgname ? `farm_name iLIKE '%%${orgname}%%'` : ""}`
+            })
+        })
+
+        const resBody = await res.json();
+        console.log(resBody);
+        if(res.status >= 200 && res.status < 400) {
+            setOrgList(resBody.data)
+        }else {
+            alert("Error: \n" + resBody.error)
+        }
+    }
+
+    return (
+        <Layout>
+            <form onSubmit={getOrg}>
+                <div>
+                    <a>Type here to filter, leave blank for no filter</a>
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search by Organization name"
+                        onChange={e => setOrgname(e.target.value)}
+                        value={orgname}
+                    />
+                </div>
+                <div>
+                    <button>Get Organizations</button>
+                </div>
+            </form>
+            {(orgList && orgList.data )? <TableView data={orgList.data} /> : <TableView data={[{"Notice":"no data to display"}]} />}
+        </Layout>
+    );
+}
+
+export default Organization;
