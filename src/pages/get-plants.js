019725d5b8d8f8d2a2da448d14004260636fce85
Aaron Gifford <gifforaa@oregonstate.edu>
2023-05-21 16:14:32 -0700

    Added ecoregions

--- src/pages/get-plants.js
+++ src/pages/get-plants.js
@@ -138,6 +138,30 @@ function Plants() {
         parksGeojson.addTo(map);
       }, [])
 
+      const [ecoregions, setEcoregions] = useState();
+
+      useEffect(() => {
+          async function fetchEcoregion() {
+              try {
+                  const res = await fetch("https://geodata.epa.gov/arcgis/rest/services/ORD/USEPA_Ecoregions_Level_III_and_IV/MapServer/7/query?where=&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=xmin%3A-126%2C+xmax%3A-115%2C+ymin%3A32%2C+ymax%3A49.1&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=8540.47801304919&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson");
+                  const data = await res.json();
+                  console.log("****DATA RETRIEVED****");
+                  console.log(data);
+                  setEcoregions(data);
+              } catch (error) {
+                  console.log("****FETCH FAILED****" + error);
+              }
+          }
+          fetchEcoregion();
+       }, []);
+  
+       useEffect(() => {
+          if(ecoregions){
+              console.log("****ECOREGION RETRIEVED****");
+              console.log(ecoregions);
+          }
+       }, [ecoregions]);
+
     // const [res, loading, error] = useAPIRequest(`https://native-plants-backend.herokuapp.com/i/INSERT INTO rev2.farms(farm_name) VALUES (%s) /${farmname_to_send}`, "POST");
     // const [res, loading, error] = useAPIRequest(`https://native-plants-backend.herokuapp.com/q/SELECT * FROM rev2.farms`, "GET");
     async function getPlants(e) {
@@ -251,6 +275,7 @@ function Plants() {
                         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
                         attribution="Tiles © <a href='https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer'>ArcGIS</a>"
                     />
+                    { (ecoregions) ? <GeoJSON data={ecoregions} /> : null }
                     {
                         SEARCH_PLANTS.map((plant) => {
                             return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,30 @@ export default function Home() {
         fetchData();
     }, []);
 
+    const [ecoregions, setEcoregions] = useState();
+
+    useEffect(() => {
+        async function fetchEcoregion() {
+            try {
+                const res = await fetch("https://geodata.epa.gov/arcgis/rest/services/ORD/USEPA_Ecoregions_Level_III_and_IV/MapServer/7/query?where=&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=xmin%3A-126%2C+xmax%3A-115%2C+ymin%3A32%2C+ymax%3A49.1&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=8540.47801304919&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson");
+                const data = await res.json();
+                console.log("****DATA RETRIEVED****");
+                console.log(data);
+                setEcoregions(data);
+            } catch (error) {
+                console.log("****FETCH FAILED****" + error);
+            }
+        }
+        fetchEcoregion();
+     }, []);
+
+     useEffect(() => {
+        if(ecoregions){
+            console.log("****ECOREGION RETRIEVED****");
+            console.log(ecoregions);
+        }
+     }, [ecoregions]);
+
     function goToGetFarms(){
         router.push('/get-farms')
     }
